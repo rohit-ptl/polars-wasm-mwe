c5052b601a81797d813fa741f799d09f56d6597b
Kival Mahadew <kivalm@protonmail.com>
2022-11-21 21:36:57 +0200

    add existing wasm + toolchain version

---
+++ rust-wasm/pkg/rust_wasm.d.ts
@@ -0,0 +1,84 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+*/
+export function init_hooks(): void;
+/**
+* @param {Uint8Array} buffer
+* @returns {string}
+*/
+export function process_file(buffer: Uint8Array): string;
+/**
+* @param {number} num_threads
+* @returns {Promise<any>}
+*/
+export function initThreadPool(num_threads: number): Promise<any>;
+/**
+* @param {number} receiver
+*/
+export function wbg_rayon_start_worker(receiver: number): void;
+/**
+*/
+export class wbg_rayon_PoolBuilder {
+  free(): void;
+/**
+* @returns {string}
+*/
+  mainJS(): string;
+/**
+* @returns {number}
+*/
+  numThreads(): number;
+/**
+* @returns {number}
+*/
+  receiver(): number;
+/**
+*/
+  build(): void;
+}
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly init_hooks: () => void;
+  readonly process_file: (a: number, b: number, c: number) => void;
+  readonly __wbg_wbg_rayon_poolbuilder_free: (a: number) => void;
+  readonly wbg_rayon_poolbuilder_mainJS: (a: number) => number;
+  readonly wbg_rayon_poolbuilder_numThreads: (a: number) => number;
+  readonly wbg_rayon_poolbuilder_receiver: (a: number) => number;
+  readonly wbg_rayon_poolbuilder_build: (a: number) => void;
+  readonly initThreadPool: (a: number) => number;
+  readonly wbg_rayon_start_worker: (a: number) => void;
+  readonly memory: WebAssembly.Memory;
+  readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
+  readonly __wbindgen_malloc: (a: number) => number;
+  readonly __wbindgen_free: (a: number, b: number) => void;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number) => number;
+  readonly __wbindgen_exn_store: (a: number) => void;
+  readonly __wbindgen_thread_destroy: () => void;
+  readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {SyncInitInput} module
+* @param {WebAssembly.Memory} maybe_memory
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: SyncInitInput, maybe_memory?: WebAssembly.Memory): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+* @param {WebAssembly.Memory} maybe_memory
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function init (module_or_path?: InitInput | Promise<InitInput>, maybe_memory?: WebAssembly.Memory): Promise<InitOutput>;
